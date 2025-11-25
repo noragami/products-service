48e0fb26036a9524486d2f3957c4263420f49bec
noragami <yuumeikai@gmail.com>
2025-11-25 15:25:58 +0100

    feat: sequelize init and config

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,19 +1,19 @@
-import { Column, Model, Table, Unique } from 'sequelize-typescript';
+import { Column, DataType, Model, Table, Unique } from 'sequelize-typescript';
 
 @Table({
 	tableName: 'products',
 })
 export class Product extends Model {
 	@Unique
-	@Column
+	@Column(DataType.STRING(50))
 	declare productToken: string;
 
-	@Column
+	@Column(DataType.STRING(100))
 	declare name: string;
 
-	@Column
+	@Column(DataType.DECIMAL(8, 2))
 	declare price: number;
 
-	@Column
+	@Column(DataType.INTEGER)
 	declare stock: number;
 }
---
+++ src/sequelize/config.js
@@ -0,0 +1,30 @@
+// src/sequelize/config.js
+
+const config = {
+  development: {
+    dialect: 'mysql',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '3306'),
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_DATABASE || 'ecommerce',
+  },
+  test: {
+    dialect: 'mysql',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '3306'),
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_DATABASE || 'ecommerce',
+  },
+  production: {
+    dialect: 'mysql',
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT || '3306'),
+    username: process.env.DB_USERNAME || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DB_DATABASE || 'ecommerce',
+  },
+};
+
+module.exports = config;
