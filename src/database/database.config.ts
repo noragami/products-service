e029d6dc06df6aadf75044bad0230b316b0dca69
noragami <yuumeikai@gmail.com>
2025-11-25 15:25:58 +0100

    feat: add sequelize + env rename

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -3,12 +3,12 @@ import { Product } from '../products/entities/product.entity';
 
 export const dataBaseConfig: SequelizeModuleOptions = {
 	dialect: 'mysql',
-	host: process.env.host ?? 'localhost',
-	port: parseInt(process.env.port || '3306'),
-	username: process.env.username ?? 'root',
-	password: process.env.password ?? 'root',
-	database: 'ecommerce',
+	host: process.env.DB_HOST ?? 'localhost',
+	port: parseInt(process.env.DB_PORT || '3306'),
+	username: process.env.DB_USERNAME ?? 'root',
+	password: process.env.DB_PASSWORD ?? 'root',
+	database: process.env.DB_DATABASE ?? 'ecommerce',
 	models: [Product],
-	autoLoadModels: true,
-	synchronize: true,
+	autoLoadModels: false,
+	synchronize: false,
 };
